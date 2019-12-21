13b2dc80dabfd4f091f0ba6c7682b98bb8e2b392
Jarod Peachey <jwpeachey107@aol.com>
2019-12-20 18:34:27 -0600

    More dynamic adding

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
+//////// Parent Elements ////////
 const navbarList = document.querySelector('ul.navbar__menu');
+const mainContent = document.querySelector('main');
+
+//////// Section Declaration ////////
+const sections = [
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismo. Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.',
+  },
+  {
+    content:
+      'Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismo. Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.',
+  },
+  {
+    content:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismo. Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu.',
+  },
+];
+
+//////// Function Declarations ////////
 
 // Create navbar title
 const createNavbarTitle = (text) => {
@@ -11,18 +31,35 @@ const createNavbarTitle = (text) => {
 // Create navbar items
 let navbarIndex = 1;
 
-const createNavbarItem = (text) => {
+const createSection = (section) => {
+  // Create elements
+  const newSection = document.createElement('section');
+  const newTitle = document.createElement('h2');
+  const newContent = document.createElement('p');
+
+  // Assign classnames and ids
+  newSection.id = `section-${section.number}`;
+  newTitle.classList.add('section__title');
+  newContent.classList.add('section__content');
+
+  // Build element
+  newContent.innerText = section.content;
+  newTitle.innerText = section.title;
+  newSection.append(newTitle, newContent);
+
+  mainContent.appendChild(newSection);
+}
+
+const createNavbarItem = (section) => {
   const navbarItem = document.createElement('li');
   navbarItem.classList.add('navbar__item');
-  navbarItem.classList.add(
-    `navbar__item-${navbarIndex}`
-  );
-  if (navbarIndex === 1) {
+  navbarItem.classList.add(`navbar__item-${section.number}`);
+  if (section.number === 1) {
     navbarItem.classList.add('navbar__item--active');
   }
   const navbarText = document.createElement('a');
-  navbarText.textContent = text;
-  navbarText.href = `#section-${navbarIndex}`;
+  navbarText.textContent = section.title;
+  navbarText.href = `#section-${section.number}`;
   navbarText.classList.add('navbar__link');
 
   navbarItem.appendChild(navbarText);
@@ -54,7 +91,7 @@ const updateActiveItem = () => {
 
 const onScroll = () => {
   const navLinks = document.querySelectorAll('a.navbar__link');
-  const currentPosition = window.scrollY;
+  const currentPosition = window.scrollY + 20;
 
   for (let i = 0; i < navLinks.length; i++) {
     const section = document.querySelector(navLinks[i].hash);
@@ -71,16 +108,18 @@ const onScroll = () => {
   }
 };
 
+//////// Element creation ////////
+
+sections.forEach((section, index) => {
+  section.number = index + 1;
+  section.title = `Section ${section.number}`;
+  createSection(section);
+  createNavbarItem(section);
+})
+
 createNavbarTitle('Home');
-createNavbarItem('One');
-createNavbarItem('Two');
-createNavbarItem('Three');
 const activeIndicator = createActiveIndicator();
 
-document.body.addEventListener('click', (event) => {
-  if (event.target.classList.contains('navbar__link')) {
-    updateActiveItem(event.target);
-  }
-});
+//////// Event listeners ////////
 
 window.addEventListener('scroll', onScroll);
